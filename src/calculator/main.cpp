7ff4e62b17caa0c0973bf54efc8df9b1ece0f606
star.barsuk <120922249+Hodilton@users.noreply.github.com>
2025-10-30 05:05:56 +0100

    pr3

--- src/calculator/main.cpp
+++
@@ -1,9 +0,0 @@
-#include <iostream>
-#include "calculator.h"
-
-int main() {
-    std::cout << "Calculator Demo (Multi-file project)" << std::endl;
-    std::cout << "5 + 3 = " << add(5, 3) << std::endl;
-    std::cout << "5 - 3 = " << subtract(5, 3) << std::endl;
-    return 0;
-}
--- src/hello.cpp
+++
@@ -1,6 +0,0 @@
-#include <iostream>
-
-int main() {
-    std::cout << "Hello, World! (Single file project or single sub-project)" << std::endl;
-    return 0;
-}
